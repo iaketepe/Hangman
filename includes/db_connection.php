06ea90b4a8c4b7ddd25ddcd97541010fa77993e7
Iyanuloluwa Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-07-03 12:14:35 -0400

    Update db_connection.php to allow for cloud deployment

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,16 +1,28 @@
 <?php
-$dsn = 'pgsql:host=localhost;port=5432;dbname=postgres';
-$username = 'postgres';
-$password = 'qwerty123';
+$databaseUrl = getenv('DATABASE_URL');  
 
-try {
-    $pdo = new PDO($dsn, $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // Optionally, test the connection
-    //echo "Connected successfully!";
-} catch (PDOException $e) {
-    // Handle connection errors
-    echo "Connection failed: " . $e->getMessage();
+if (!$databaseUrl) {
+    die('DATABASE_URL environment variable is not set.');
+}
+
+if (preg_match("/postgres:\/\/(.*):(.*)@(.*):(\d+)\/(.*)/", $databaseUrl, $matches)) {
+    $username = $matches[1];
+    $password = $matches[2];
+    $host = $matches[3];
+    $port = $matches[4];
+    $dbname = $matches[5];
+
+    $dsn = 'pgsql:host=' . $host . ';port=' . $port . ';dbname=' . $dbname;
+
+    try {
+        $pdo = new PDO($dsn, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo "Connected successfully!";
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+} else {
+    die('Invalid DATABASE_URL format.');
 }
 
 ?>
